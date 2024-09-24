442e3946e45e747364fc06b5752fa78bfa7cb468
Justin Battrick <justin.bat19@gmail.com>
2024-09-24 17:57:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- The 100 The Text Game ---");
+Console.WriteLine();
+
+Console.WriteLine("Scene: Clarke Griffin (you) sits at a table in the Mess Hall of the Ark with her best friend Wells Jaha.");
+Console.WriteLine();
+
