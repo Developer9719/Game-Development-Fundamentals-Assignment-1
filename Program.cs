7f1140c740c03261888bcb4061049e0f04c10c14
Justin Battrick <justin.bat19@gmail.com>
2024-09-24 18:43:36 -0400

    Scene 2 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
 Console.WriteLine("--- The 100 The Text Game ---");
 Console.WriteLine();
 // Scene 1
-Console.WriteLine("Scene: Clarke Griffin (you) sits at a table in the Mess Hall of the Ark with her best friend Wells Jaha eating lunch.");
+Console.WriteLine("--- Clarke Griffin (you) sits at a table in the Mess Hall of the Ark with her best friend Wells Jaha eating lunch. ---");
 Console.WriteLine();
 
 Console.WriteLine("Wells: Hows your sandwich today?");
@@ -26,5 +26,23 @@ Console.WriteLine("Wells: Your secrets safe with me Clarke.");
 Console.WriteLine();
 
 // Scene 2
+Console.WriteLine("--- THE NEXT DAY ---");
+Console.WriteLine();
 
+Console.WriteLine("Scene: Clarke Griffin (you) walks in on her Dad recording a video to play throughout the Ark explaining the life support falure and asking for help.");
+Console.WriteLine();
 
+Console.WriteLine("Clarke: Dad! What are you doing?! You can't send that video out. You know they will float you.");
+Console.WriteLine();
+Console.WriteLine("--- Jake Griffin (Clarkes Dad) stares blankly at Clarke ---");
+Console.WriteLine("Clarke: Dad, I know. I overheard you and Mom talking.");
+Console.WriteLine("Jake: Ok so you know. Clarke I have to do this. They need to know. I feel like this could bring out the best in all of us and we can all work toward a solution together.");
+Console.WriteLine("Clarke: So what are you planning? Are you gonna break into the comms system and play your video to all of the Ark?");
+Console.WriteLine("Dad, no! Please.");
+Console.WriteLine();
+Console.WriteLine("--- Just then 6 gaurds break in and haul Jake away. ---");
+Console.WriteLine("Gaurd: Jake Griffin you are under arrest for treason.");
+Console.WriteLine("--- As the gaurds pull Jake out of the room, 2 of the gaurds take control over a fighting Clarke, stopping her from interfering with the arrest");
+Console.WriteLine("--- They pull Jake into the airlock and put clarke into a Skybox jail cell. With the press of a button, Jake is gone, ejected into the dark void of space, dead.");
+
+// Scene 3
