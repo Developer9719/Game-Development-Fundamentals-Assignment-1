9cd0a5f3d83a42d93dd1a1e6b36cb42694a2f17b
Justin Battrick <justin.bat19@gmail.com>
2024-09-24 18:22:11 -0400

    Scene 1 done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/* Inspiration:
+ * https://youtube.com/watch?v=1PDkwZeUWh8&list=PLlLacQjlPI0pRy2siKx024PygHZFQWuj7&index=36
+ * https://www.youtube.com/watch?v=d6Si3GAPeXY
+ */
 Console.WriteLine("--- The 100 The Text Game ---");
 Console.WriteLine();
-
-Console.WriteLine("Scene: Clarke Griffin (you) sits at a table in the Mess Hall of the Ark with her best friend Wells Jaha.");
+// Scene 1
+Console.WriteLine("Scene: Clarke Griffin (you) sits at a table in the Mess Hall of the Ark with her best friend Wells Jaha eating lunch.");
 Console.WriteLine();
+
+Console.WriteLine("Wells: Hows your sandwich today?");
+Console.WriteLine("Clarke: Disgusting.");
+Console.WriteLine("Wells: You say that everyday.");
+Console.WriteLine("Clarke: Well, it is.");
+Console.WriteLine("Wells: Are you okay? You seem distracted today.");
+Console.WriteLine("Clarke: Actually I am.");
+Console.WriteLine("Clarke: I overheard my parents talking earlier. Apparently there is something wrong with the life support systems on the Ark.");
+Console.WriteLine("Wells: What do you mean?");
+Console.WriteLine("Clarke: I don't know, I'm not even supposed to know, but apparently there is only a few months of oxygen left up here.");
+Console.WriteLine("Wells: Clarke, this is not the first time they've come across some kind of issue with the ship. The counsil always finds a way to fix it.");
+Console.WriteLine("Clarke: I know, but from the sounds of it they might not be able to fix it this time. My dad seem pretty nervous. He's talking about going public with the info and hoping all the citizens of the Ark can put there minds to a solution.");
+Console.WriteLine("Wells: Clarke he can't do that. He'd get floated.");
+Console.WriteLine("Clarke: I know. Wells you can't tell anyone about this.");
+Console.WriteLine("Wells: Your secrets safe with me Clarke.");
+Console.WriteLine();
+
+// Scene 2
+
 
